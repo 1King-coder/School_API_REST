be2bbf969911c78ed5049b2a114849fcef9f38ab
1King-coder <progvitor@gmail.com>
2021-08-01 22:20:14 -0300

    adding Cors

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,7 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
 var _path = require('path');
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 
 _dotenv2.default.config();
 
@@ -13,6 +15,21 @@ var _tokenR = require('./routes/tokenR'); var _tokenR2 = _interopRequireDefault(
 var _studentR = require('./routes/studentR'); var _studentR2 = _interopRequireDefault(_studentR);
 var _uploadR = require('./routes/uploadR'); var _uploadR2 = _interopRequireDefault(_uploadR);
 
+const whiteList = [
+  'http://54.232.45.47',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if ((whiteList).indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 class App {
   constructor() {
     this.app = _express2.default.call(void 0, );
@@ -21,6 +38,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(_cors2.default.call(void 0, corsOptions));
+    this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, 'uploads')));
