a7f6dbb077401ce4d122e776cfe129e3de133077
1King-coder <progvitor@gmail.com>
2021-08-16 12:24:28 -0300

    'test'

--- src/app.js
+++ src/app.js
@@ -16,7 +16,6 @@ import studentRoutes from './routes/studentR';
 import uploadRoutes from './routes/uploadR';
 
 const whiteList = [
-  'https://pt.wikipedia.org',
   'http://54.232.12.74',
   'http://localhost:3000',
 ];
